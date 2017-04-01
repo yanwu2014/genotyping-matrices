d3220a6dc7d93da126e75035df0d6f13c98b31b7
Yan Wu <yauwning@gmail.com>
2017-04-01 12:58:56 -0700

    added filtering for reads per UMI

--- plotUMIdist.py
+++ plotUMIdist.py
@@ -11,15 +11,25 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import spacerCalling as sc
 
+cell_barcodes_file = sys.argv[1]
+minUMIReads = int(sys.argv[2])
+
 with open(sys.argv[1]) as f:
     cell_barcodes = cp.load(f)
 
-for k in cell_barcodes:
-    cell_barcodes[k].countBarcodes(minFrac = 0)
+umi_reads = []
+for k,c in cell_barcodes.items():
+    totalReads = c.gbc_reads
+    umis = set()
+    for gbcDict in c.molTags.values():
+        for umi,counts in gbcDict.items():
+            if not umi in umis:
+                umi_reads.append(counts)
+            umis.add(umi)
+    cell_barcodes[k].countBarcodes(minReads = minUMIReads)
 
 read_fracs = []
 umi_fracs = []
-
 for k,c in cell_barcodes.items():
     totalReads = np.sum(c.gbcReadCounts.values())
     totalUMIs = np.sum(c.gbcUMICounts.values())
@@ -27,14 +37,21 @@ for k,c in cell_barcodes.items():
         umis = c.gbcUMICounts[gbc]
         read_fracs.append(float(reads)/totalReads)
         umi_fracs.append(float(umis)/totalUMIs)
-
+    
 read_fracs = np.array(read_fracs)
 umi_fracs = np.array(umi_fracs)
 
-sns.jointplot(x = read_fracs, y = umi_fracs, kind = 'hex', bins = 'log', gridsize = 25,
+sns.jointplot(x = read_fracs, y = umi_fracs, kind = 'hex', gridsize = 25,
               stat_func = None)
 
 fig_name = sys.argv[1].replace('_cell_barcodes.pickle', '_umi_vs_read_frac.png')
 plt.savefig(fig_name, bbox_inches = 'tight')
 plt.clf()
+
+fig_name = sys.argv[1].replace('_cell_barcodes.pickle', '_umi_read_dist.png')
+sns.distplot(np.log(umi_reads))
+plt.savefig(fig_name, bbox_inches = 'tight')
+plt.clf()
+
+
 
