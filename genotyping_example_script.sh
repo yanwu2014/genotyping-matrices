ba880fa32b2da11a28c28886b7bd52f7708c8cef
Yan Wu <yauwning@gmail.com>
2017-11-10 14:43:04 -0800

    Moved barcode handles and length to command line arguments, added example shell script

---
+++ genotyping_example_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+matrixName=
+fastqName=
+barcodeToGene=
+
+# Convert from fastq to bam	
+java -Xmx8g -jar $HOME/Yan_SSD/PicardTools/picard.jar FastqToSam F1=$fastqName\_R2.fastq.gz O=$fastqName\_R2.bam SM=$fastqName SORT_ORDER=unsorted TMP_DIR=tmp/
+
+# Tag Read2 bam with Read1 cell/molecule barcodes
+python ~/genotyping-matrices/convert_gRNA.py $fastqName\_R1.fastq.gz $fastqName\_R2.bam 
+samtools index $fastqName\_R2.tagged.bam
+
+# Parse cell barcodes and store in python dictionary
+python $HOME/genotyping-matrices/parseCellBarcodes.py $matrixName $fastqName\_R2.tagged.bam 20 'GGCTGTTACGCG' 'CTACTGAC'
+ 
+# Plot distribution of reads per UMI in order to set appropriate thresholds
+python $HOME/genotyping-matrices/plotUMIdist.py $fastqName\_R2_cell_barcodes.pickle 0.01
+
+# Genotype cells: outputs a genotype dictionary in csv format
+python $HOME/genotyping-matrices/genotypeCells.py $matrixName $fastqName\_R2_cell_barcodes.pickle $barcodeToGene 0.1 0.1 0.01
--- parseCellBarcodes.py
+++ parseCellBarcodes.py
@@ -9,16 +9,24 @@ import pandas as pd
 
 dataFrameFile = sys.argv[1]
 bamFile = sys.argv[2]
-cbc_edit_dist = int(sys.argv[3]) # Edit distance between cells
+BARCODE_LENGTH = int(sys.argv[3])
+BC_START_HANDLE = sys.argv[4]
+BC_END_HANDLE = sys.argv[5]
+
+cbc_edit_dist = 1 # Edit distance between cells
 gbc_edit_dist = 1
 
-BARCODE_LENGTH = 20
+#BARCODE_LENGTH = 20
+
+## gRNA construct handles
 #BC_START_HANDLE = 'CCGAGTCGGTGC'
-#BC_END_HANDLE = 'TATGA' 
-BC_START_HANDLE = 'GGCTGTTACGCG'
-BC_END_HANDLE = 'CTACTGAC'
+#BC_END_HANDLE = 'TATGA'
 
+## ORF overexpression handles
+#BC_START_HANDLE = 'GGCTGTTACGCG'
+#BC_END_HANDLE = 'CTACTGAC'
 
+# Load scRNA-seq counts matrix
 df = pd.read_csv(dataFrameFile, sep = '\t', header = 0, index_col = 0)
 
 # Strip batch ids
@@ -28,13 +36,16 @@ df.columns = cell_barcodes
 # Get cell names and total UMIs
 coreCells = df.sum(0).to_dict()
 
+# Parse cell barcodes
 cellBarcodes = sc.getCellBarcodes(bamFile, coreCells, BARCODE_LENGTH, BC_START_HANDLE,
                                   BC_END_HANDLE, edit_dist = cbc_edit_dist)
+
+# Collapse barcodes within edit distance
 for cbc,cell in cellBarcodes.items():
     cellBarcodes[cbc].collapseMolTags(cbc_edit_dist)
 
+# Write barcodes dictionary to pickled object
 barcodeFile = bamFile.replace('.tagged.bam', '_cell_barcodes.pickle')
 with open(barcodeFile, 'w') as f:
     cp.dump(cellBarcodes,f)
-
 
